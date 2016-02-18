ba37ce12cc51de82dde5253be90502841ba89026
Arrray <gjc@hrsoft.net>
2016-02-18 17:51:23 +0800

    mr修改

--- mr/01/index.php
+++ mr/01/index.php
@@ -1,9 +1,11 @@
 <?php
-	session_start();
-	header('Content-Type:text/html;charset=gb2312');
+	session_start();   //开启session支持
+	header('Content-Type:text/html;charset=gb2312');    //设置页面编码
+//	判断cookie是否为空
 	if(!empty($_COOKIE['name']) and !is_null($_COOKIE['name'])){
-		$_SESSION['name'] = $_COOKIE['name'];
-		header('location:http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']).'/main.php');
+		$_SESSION['name'] = $_COOKIE['name'];    //将cookie保存到session中
+//		跳转到main.php页
+		header('location:http://localhost/PHPpractice/mr/05/01/main.php');
 	}else{
 		header('location:http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']).'/login.php');
 	}
